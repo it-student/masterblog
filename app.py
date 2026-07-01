fb498560065ede27d59c03369172da2c2f8a79c6
Yasin Akkus <akkusoft@gmail.com>
2026-07-01 10:59:56 +0200

    created folder structure inheriting our first template index.html, as well as our initial style.css. Also our Database is created as db.json as per instruction. Now our base route opens the db.json file and reads all, hands it over to our template, which then iterates over the given data displaying it in the resulting final html-file.

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+import json
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+template_blog_posts = [
+    {"id": 1, "author": "John Doe", "title": "First Post", "content": "This is my first post."},
+    {"id": 2, "author": "Jane Doe", "title": "Second Post", "content": "This is another post."}
+    # More blog posts can go here...
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    """
+    Base route for all blog posts to be presented.
+    """
+    # add code here to fetch the blog posts from a file
+    with open('db.json', 'r', encoding='utf-8') as db_file:
+        blog_posts = json.load(db_file)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
