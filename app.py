f6eb0254d02fbfa7d616b7b711d6d8f0fe28f158
Yasin Akkus <akkusoft@gmail.com>
2026-07-02 15:50:49 +0200

    added functionality for the route '/add' to save a post retrieving the information from the form inputs delivered from the POST method and an id from the db itself to save the new post with a valid unique id inside the db correctly. therefore added also new methods simulating SQL-Database functionality 'fetch_all' (SELECT * FROM ...) and 'insert_into' (INSERT INTO ...)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, url_for, request, render_template, redirect
 
 app = Flask(__name__)
 
@@ -10,6 +10,43 @@ template_blog_posts = [
     # More blog posts can go here...
 ]
 
+def get_db() -> dict:
+    """
+    Opens the db.JSON file and returns all entries as a dict.
+    :return db_file: dict with all entries.
+    """
+    with open('db.json', 'r', encoding='utf-8') as db_file:
+        return json.load(db_file)
+
+def get_next_id() -> int:
+    """
+    Gets the next uninstantiated id from the db.JSON file and returns it.
+    :return next_id: next uninstantiated id from the db.JSON file:
+    """
+    json_db = get_db()
+    return json_db['next_id']
+
+def fetch_all():
+    """
+    Simulates a SELECT * FROM db.JSON file and returns all entries as a dict.
+    :return:
+    """
+    json_db = get_db()
+    return json_db['data']
+
+def insert_into(blog_post: dict) -> None:
+    """
+    Simulates a INSERT into db.JSON file.
+    :param blog_post:
+    :return None:
+    """
+    json_db  = get_db()
+    json_db['data'].append(blog_post)
+    json_db['next_id'] += 1
+    with open('db.json', 'w', encoding='utf-8') as db_file:
+        db_file.write(json.dumps(json_db))
+    print(f'Blog post {blog_post['title']} saved to db.')
+    return None
 
 @app.route('/')
 def index():
@@ -17,9 +54,24 @@ def index():
     Base route for all blog posts to be presented.
     """
     # add code here to fetch the blog posts from a file
-    with open('db.json', 'r', encoding='utf-8') as db_file:
-        blog_posts = json.load(db_file)
+    blog_posts = fetch_all()
     return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    """
+    Route for adding a new blog post.
+    On 'GET' the form will be served, a new entry will be added otherwise.
+    """
+    if request.method == 'POST':
+        new_post = {'id': get_next_id(),
+                    'author': request.form['author'],
+                    'title': request.form['title'],
+                    'content': request.form['content'],}
+        insert_into(new_post)
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
